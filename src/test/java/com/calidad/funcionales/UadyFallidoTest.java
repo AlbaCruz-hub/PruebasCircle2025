fe7b830d2e262d0aac4614adec6774313e73ba0d
AlbaCruz-hub <gabis200314@gmail.com>
2025-12-07 21:53:02 -0600

    Cambios en funcionales

--- src/test/java/com/calidad/funcionales/UadyFallidoTest.java
+++ src/test/java/com/calidad/funcionales/UadyFallidoTest.java
@@ -2,7 +2,6 @@ package com.calidad.funcionales;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,77 +17,97 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class UadyFallidoTest {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
-  JavascriptExecutor js;
-  @BeforeEach
-  public void setUp() throws Exception {
-    WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver();
-    baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-    js = (JavascriptExecutor) driver;
-  }
 
-  @Test
-  public void testAccesoUADY() throws Exception {
-    driver.get("https://es.uadyvirtual.uady.mx/login/index.php");
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("V1n9y8jr");
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("V1n9y8jr");
-    driver.findElement(By.id("region-main-box")).click();
-    driver.findElement(By.id("region-main-box")).click();
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("1234gdg");
-    driver.findElement(By.id("loginbtn")).click();
-  }
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+    JavascriptExecutor js;
 
-  @AfterEach
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
+    @BeforeEach
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--headless=new");  // ✅ ELIMINADO
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        js = (JavascriptExecutor) driver;
     }
-  }
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
+    @Test
+    public void testAccesoUADY() throws Exception {
+        driver.get("https://es.uadyvirtual.uady.mx/login/index.php");
+
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("V1n9y8jr");
+
+        driver.findElement(By.id("region-main-box")).click();
+        driver.findElement(By.id("region-main-box")).click();
+
+        Thread.sleep(5000);
+
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("1234gdg");
+
+        driver.findElement(By.id("loginbtn")).click();
     }
-  }
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    @AfterEach
+    public void tearDown() throws Exception {
+        if (driver != null) {
+            driver.quit();
+        }
+
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
     }
-  }
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
-  }
 
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
 
 }
